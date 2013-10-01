4dd1733ecca9031666728d785a10f6566002a669
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-10-01 08:32:36 +0200

    add complex loans

--- src/main/scala/fr/xebia/scalaio/iteratee/Loan.scala
+++ src/main/scala/fr/xebia/scalaio/iteratee/Loan.scala
@@ -47,4 +47,43 @@ object Loan {
     )
   }
 
+  def complex(
+               start: DateMidnight,
+               initial: Amount,
+               duration: Int,
+               fixedRate: Double) = {
+
+    val sleepTime = ((Math.random() * 1000) % 2) toLong
+
+    Loan(
+      initial,
+      duration,
+      RowIt(first = new Row {
+
+        lazy val date = start + (1 year)
+
+        lazy val interests = initial * fixedRate
+
+        lazy val amortization = initial / duration
+
+        lazy val outstanding = initial - amortization
+
+      },
+        op = (last: Row) => new Row {
+
+          lazy val date = last.date + (1 year)
+
+          lazy val interests = {
+            Thread.sleep(sleepTime)
+            last.outstanding * fixedRate
+          }
+
+          lazy val amortization = last amortization
+
+          lazy val outstanding = last.outstanding - amortization
+
+        })
+    )
+  }
+
 }
--- src/main/scala/fr/xebia/scalaio/iteratee/Master.scala
+++ src/main/scala/fr/xebia/scalaio/iteratee/Master.scala
@@ -6,7 +6,7 @@ object Master extends App {
   import java.util.Currency
   import org.joda.time.DateMidnight
 
-  val loans =
+  val simpleLoans =
     for (i <- (0 to 5000).toSeq)
     yield {
       val rand = 1 + i % 31
@@ -18,12 +18,32 @@ object Master extends App {
       )
     }
 
-  val portfolio = Portfolio(loans)
+  val complexLoans =
+    for (i <- (0 to 5000).toSeq)
+    yield {
+      val rand = 1 + i % 31
+      Loan.complex(
+        start = DateMidnight.parse("2012-01-" + rand),
+        initial = Amount(1000, Currency.getInstance("EUR")),
+        duration = 50,
+        fixedRate = rand / 100
+      )
+    }
+
+  val simplePortfolio = Portfolio(simpleLoans)
+
+  val complexPortfolio = Portfolio(complexLoans)
 
   val start = System.currentTimeMillis()
 
-  LogTime {
-    portfolio.rows.toStream
+  LogTime("simple ") {
+    simplePortfolio.rows.toStream
+      .map(row => row.interests + row.amortization)
+      .foldLeft(Amount(0, Currency.getInstance("EUR")))(_ + _)
+  }
+
+  LogTime("complex") {
+    complexPortfolio.rows.toStream
       .map(row => row.interests + row.amortization)
       .foldLeft(Amount(0, Currency.getInstance("EUR")))(_ + _)
   }
@@ -32,10 +52,10 @@ object Master extends App {
 
 
 object LogTime {
-  def apply[T](t: => T): T = {
+  def apply[T](name: String)(t: => T): T = {
     val start = System.currentTimeMillis()
     val result = t
-    println(s"${System.currentTimeMillis() - start} ms")
+    println(s"[$name] : ${System.currentTimeMillis() - start} ms")
     result
   }
 }
